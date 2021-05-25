aeb60c77b7ec248c78a12b114e982ff5c962e582
Yashu812 <79332515+Yashu812@users.noreply.github.com>
2021-05-25 19:35:04 +0530

    Update routes.py
    Updated for 'Wedding' and 'Conference And Seminar' pages.

--- Events/routes.py
+++ Events/routes.py
@@ -3,6 +3,8 @@ from Events import app
 from flask import render_template,request,url_for,redirect
 
 bevent=[]
+wevent=[]
+csevent=[]
 POSTGRESQL_URI="postgres://qaooeuge:3ssa-7dQyeAogu6uQ9OeWtDhVkIv8a9r@topsy.db.elephantsql.com:5432/qaooeuge"
 connection=psycopg2.connect(POSTGRESQL_URI)
 try:
@@ -79,7 +81,21 @@ def rec():
                 bevent = cursor.fetchall()
     return render_template('receipt.jinja2', evententries=bevent)
 
+@app.route('/receiptw')
+def recw():
+    with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM WEDTB order by twid desc limit 1;")
+                wevent = cursor.fetchall()
+    return render_template('receiptw.jinja2', evententries=wevent)
 
+@app.route('/receiptcs')
+def reccs():
+    with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM CSTB order by tcid desc limit 1;")
+                csevent = cursor.fetchall()
+    return render_template('receiptcs.jinja2', evententries=csevent)
 
 @app.route('/Login', methods=["GET","POST"])
 def log():
@@ -107,12 +123,85 @@ def log():
 def ty():
     return render_template('type.html')
 
-@app.route('/ConferenceAndSeminar')
+@app.route('/ConferenceAndSeminar', methods=["GET", "POST"])
 def ConferenceAndSeminar():
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT tcid FROM CSTB order by tcid desc limit 1;")
+                test = []
+                test = cursor.fetchall()
+                num = test[0]
+                s1 = num[0]
+                s2 = 'C'
+                if s1.startswith(s2):
+                    s3 = s1.replace(s2, '')
+                imp = int(s3)+1
+    except:
+        imp=1
+    if request.method == "POST":
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO CSTB VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
+                            (
+                                'C'+str(imp),
+                                request.form.get("ctitle"),
+                                request.form.get("ctype"),
+                                request.form.get("cdate"),
+                                request.form.get("ctime"),
+                                request.form.get("cspsname"),
+                                request.form.get("cspkname"),
+                                request.form.get("cbudget"),
+                                request.form.get("cguests"),
+                                request.form.get("ccate"),
+                                request.form.get("cservices"),
+                                request.form.get("cemail"),
+                                request.form.get("cphone"),
+                                request.form.get("cappdate"),
+                                request.form.get("capptime"),
+                            )
+                )
+        return redirect(url_for('reccs'))
     return render_template('ConferenceAndSeminar.html')
 
-@app.route('/wedding')
+@app.route('/wedding', methods=["GET", "POST"])
 def wedding():
+    try:
+        with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT twid FROM WEDTB order by twid desc limit 1;")
+                    test = []
+                    test = cursor.fetchall()
+                    num = test[0]
+                    s1 = num[0]
+                    s2 = 'W'
+                    if s1.startswith(s2):
+                        s3 = s1.replace(s2, '')
+                    imp = int(s3)+1
+    except:
+        imp=1
+    if request.method == "POST":
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO WEDTB VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
+                            (
+                                'W'+str(imp),
+                                request.form.get("wname"),
+                                request.form.get("wvenue"),
+                                request.form.get("wdate"),
+                                request.form.get("wtime"),
+                                request.form.get("wguests"),
+                                request.form.getlist("check1"),
+                                request.form.get("writuals"),
+                                request.form.getlist("check2"),
+                                request.form.get("wtheme"),
+                                request.form.get("wemail"),
+                                request.form.get("wphone"),
+                                request.form.get("wappdate"),
+                                request.form.get("wapptime"),
+                            )
+                )
+        return redirect(url_for('recw'))
     return render_template('wedding.html')
 
 @app.route('/Virtual events', methods=["GET","POST"])
